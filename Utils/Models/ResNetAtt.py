036d88e66b5d9c529f10beaa3bb96ae772cbdaf4
anonym15621 <125087626+anonym15621@users.noreply.github.com>
2023-02-16 19:30:31 +0100

    Update ResNetAtt.py

--- Utils/Models/ResNetAtt.py
+++ Utils/Models/ResNetAtt.py
@@ -2,60 +2,59 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-class Conv1dSame(torch.nn.Module):
-    def __init__(self, in_channels, out_channels, kernel_size, dilation=1, bias=True,
-                 padding_layer=torch.nn.ReflectionPad1d):
-        super().__init__()
-        ka = kernel_size // 2
-        kb = ka - 1 if kernel_size % 2 == 0 else ka
-        
-        if dilation > 1:
-            ka = ka * dilation
-            kb = kb * dilation
-        
-        self.net = torch.nn.Sequential(padding_layer((ka,kb)),
-                                       torch.nn.Conv1d(in_channels, out_channels, kernel_size, 
-                                                       dilation=dilation, bias=bias)
-                                       )
+class Conv1dSamePadding(nn.Conv1d):
+    def forward(self, input):
+        return conv1d_same_padding(input, self.weight, self.bias, self.stride,
+                                   self.dilation, self.groups)
 
-    def forward(self, x):
-        return self.net(x)
 
-class DilatedResUnit(nn.Module):       
-    def __init__(self, c_in, c_out, k=3, dilation=1, dp_rate=0.1):
-        super(DilatedResUnit,self).__init__()
-        
-        self.convdilatedlayers = nn.Sequential(Conv1dSame(in_channels=c_in, out_channels=c_out, 
-                                                          kernel_size=k, dilation=dilation),
-                                               nn.ReLU(),
-                                               nn.BatchNorm1d(c_out),
-                                               nn.Dropout(dp_rate)
-                                               ) 
-    def forward(self,x):
-        new_x = self.convdilatedlayers(x)
-        return x + new_x
+def conv1d_same_padding(input, weight, bias, stride, dilation, groups):
+    kernel, dilation, stride = weight.size(2), dilation[0], stride[0]
+    l_out = l_in = input.size(2)
+    padding = (((l_out - 1) * stride) - l_in + (dilation * (kernel - 1)) + 1)
+    if padding % 2 != 0:
+        input = F.pad(input, [0, 1])
 
-class ResUnit(nn.Module):
-    def __init__(self, c_in, c_out, k=3, dp_rate=0.1):
+    return F.conv1d(input=input, weight=weight, bias=bias, stride=stride,
+                    padding=padding // 2,
+                    dilation=dilation, groups=groups)
+
+class ResUnit(nn.Module):       
+    def __init__(self, c_in, c_out, k=8, dilation=1, stride=1, bias=True, dp_rate=0.1):
         super(ResUnit,self).__init__()
-        self.convlayers = nn.Sequential(Conv1dSame(in_channels=c_in, out_channels=c_out, kernel_size=k),
-                                        nn.ReLU(),
-                                        nn.BatchNorm1d(c_out),
-                                        nn.Dropout(dp_rate)
-                                        ) 
+        
+        self.layers = nn.Sequential(Conv1dSamePadding(in_channels=c_in, out_channels=c_out,
+                                                      kernel_size=k, dilation=dilation, stride=stride, bias=bias),
+                                    nn.ReLU(),
+                                    nn.BatchNorm1d(c_out),
+                                    nn.Dropout(dp_rate)
+                                    )
+        if c_in > 1 and c_in!=c_out:
+            self.match_residual=True
+            self.conv = nn.Conv1d(in_channels=c_in, out_channels=c_out, kernel_size=1)
+        else:
+            self.match_residual=False
+            
     def forward(self,x):
-        new_x = self.convlayers(x)
-        return x + new_x
-    
+        if self.match_residual:
+            x_bottleneck = self.conv(x)
+            x = self.layers(x)
+            
+            return torch.add(x_bottleneck, x)
+        else:
+            return torch.add(x, self.layers(x))
+
 class DilatedBlock(nn.Module):  
-    def __init__(self, c_in=24, c_out=24, kernel_size=3, dilation_list=[1, 2, 4, 8], dp_rate=0.1):
+    def __init__(self, c_in=24, c_out=24, 
+                 kernel_size=8, dilation_list=[1, 2, 4, 8], dp_rate=0.1):
         super(DilatedBlock,self).__init__()
  
         layers = []
-        
         for i, dilation in enumerate(dilation_list):
-            layers.append(DilatedResUnit(c_in[i], c_out, kernel_size, dilation, dp_rate))
-            
+            if i==0:
+                layers.append(ResUnit(c_in, c_out, k=kernel_size, dilation=dilation, dp_rate=dp_rate))
+            else:
+                layers.append(ResUnit(c_out, c_out, k=kernel_size, dilation=dilation, dp_rate=dp_rate))
         self.network = torch.nn.Sequential(*layers)
             
     def forward(self,x):
@@ -63,13 +62,13 @@ class DilatedBlock(nn.Module):
         return x
     
 class AttentionBlock(nn.Module):  
-    def __init__(self, c_in=24, c_out=24, kernel_size=3, dp_rate=0.1):
+    def __init__(self, c_in=24, c_out=24, kernel_size=8, dp_rate=0.1):
         super(AttentionBlock,self).__init__()
         
-        self.ResUnit1 = ResUnit(c_in, c_out)
-        self.ResUnit2 = ResUnit(c_in, c_out)
-        self.ResUnit3 = ResUnit(c_in, c_out)
-        self.ResUnit4 = ResUnit(c_in, c_out)
+        self.ResUnit1 = ResUnit(c_in, c_out, k=kernel_size)
+        self.ResUnit2 = ResUnit(c_in, c_out, k=kernel_size)
+        self.ResUnit3 = ResUnit(c_in, c_out, k=kernel_size)
+        self.ResUnit4 = ResUnit(c_in, c_out, k=kernel_size)
         
         self.max_pool1 = nn.MaxPool1d(kernel_size, return_indices=True)
         self.max_pool2 = nn.MaxPool1d(kernel_size, return_indices=True)
@@ -99,33 +98,50 @@ class AttentionBlock(nn.Module):
 class ResNetAtt(nn.Module):
     """
     Model from "Residential Appliance Detection Using Attentionbased Deep Convolutional Neural Network" paper.
+    
+    Inputs :
+    - MTS like [Batch, M, Length]
+    - Output : [Batch, nb_class]
+    
+    Paper authour's parameters :
+        - Dilate blocks # 6
+        - Dilation rate {1, 2, 4, 8}
+        - Attention module # 2
+        - Kernel size 8
+        - Kernel number 24
+        - d_ff (First fully connected) 100
+        - Batch size 32
+        - Dropout rate 0.1
+        - Optimizing method AMSGrad
+        - Start learning rate 0.0002
+        - Learning rate decay 0.5
+        - Training stop early stopping
     """
-    def __init__(self, c_in=1, n_dilated_block=6, n_attention_block=2, in_model_channel=24, 
-                 kernel_size=8, dilation_list=[1, 2, 4, 8], d_ff=256, dp_rate=0.2, nb_class=2):
+    def __init__(self, in_channels=1, n_dilated_block=6, n_attention_block=2, in_model_channel=24, 
+                 kernel_size=8, dilation_list=[1, 2, 4, 8], d_ff=100, dp_rate=0.1, nb_class=2):
         super(ResNetAtt, self).__init__()
         
         layers = []
-        
         for i in range(n_dilated_block):
-            in_channel = []
-            if i==0:
-                in_channel.append(c_in)
-                in_channel = in_channel + [in_model_channel for i in range(len(dilation_list)-1)]
-            else:
-                in_channel = [in_model_channel for i in range(len(dilation_list))]
+            layers.append(DilatedBlock(c_in=in_channels if i==0 else in_model_channel, 
+                                       c_out=in_model_channel, kernel_size=kernel_size, 
+                                       dilation_list=dilation_list, dp_rate=dp_rate))
             
-            layers.append(DilatedBlock(in_channel, in_model_channel, kernel_size, dilation_list, dp_rate))
+        self.res_network = torch.nn.Sequential(*layers)    
             
+        layers = []
         for i in range(n_attention_block):
-            layers.append(AttentionBlock(in_model_channel, in_model_channel, kernel_size, dp_rate))
+            layers.append(AttentionBlock(c_in=in_model_channel, c_out=in_model_channel, 
+                                         kernel_size=kernel_size, dp_rate=dp_rate))
             
-        self.blocks_network = torch.nn.Sequential(*layers)
+        self.att_network = torch.nn.Sequential(*layers)
         
         self.linear1 = nn.LazyLinear(d_ff)
         self.linear2 = nn.Linear(d_ff, nb_class)
 
     def forward(self, x) -> torch.Tensor:
-        x = self.blocks_network(x)
+        x = self.res_network(x)
+        x = self.att_network(x)
         x = F.relu(self.linear1(torch.flatten(x, 1)))
 
         return self.linear2(x)
